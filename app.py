b10ce2d71dfe02a5be34ee27f310025618bd64fa
leticiaOntiveros <leticiaontiveros41@gmail.com>
2025-04-07 12:58:21 -0600

    conf. en la bd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from auth.auth import auth_bp  # Importa el Blueprint de autenticación
 app = Flask(__name__)
 
 # Configuración de la aplicación
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://aptoladmin:apptol123@localhost:3306/apptol'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://aptoladmin:apptol123@theoriginallab_apptol-db:3306/apptol'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret-key'  # Clave secreta para JWT
 
