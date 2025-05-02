d965b806c8760f51ce48373b5837c31a8ae007d0
leticiaOntiveros <leticiaontiveros41@gmail.com>
2025-05-02 11:12:07 -0600

    mod. name BD

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120),  nullable=False)
     name = db.Column(db.String(120), nullable=False)
-    token = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), nullable=True)
     profile_img = db.Column(db.String(120), nullable=True)
     token = db.Column(db.String(120))
