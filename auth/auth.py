598232789e8993236cb9ebdaba1e230f79d3cd7e
leticiaOntiveros <leticiaontiveros41@gmail.com>
2025-05-02 12:20:00 -0600

    mod 5

--- auth/auth.py
+++ auth/auth.py
@@ -34,9 +34,9 @@ def register():
     email = data['email'].lower()
     name = data['name']
     password = data['password']
-    if data.get('phone'):
+    if data.get('phone') and data['phone'] != "":
         phone = data['phone']
-    if data.get('profile_img'):
+    if data.get('profile_img') and data['profile_img'] != "":
         profile_img = data['profile_img']
    
 
